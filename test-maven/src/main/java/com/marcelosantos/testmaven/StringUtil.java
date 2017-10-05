ee1c6fa46949d63264b83b46f878cb1af7c52465
Marcelo Luis dos Santos <marcelo@connectedinteractive.com>
2017-10-05 17:00:54 -0300

    first commit

---
+++ test-maven/src/main/java/com/marcelosantos/testmaven/StringUtil.java
@@ -0,0 +1,12 @@
+package com.marcelosantos.testmaven;
+
+/**
+ * Created by marcelosantos on 05/10/17.
+ */
+
+public class StringUtil {
+
+    public String returnAppendedString(String stringToAppend) {
+        return "test_maven " + stringToAppend;
+    }
+}
